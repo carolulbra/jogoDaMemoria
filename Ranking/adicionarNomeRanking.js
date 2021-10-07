d3102043cbe1544bc216233a35547910f5ad4b6a
carolulbra <91291402+carolulbra@users.noreply.github.com>
2021-10-07 20:15:50 -0300

    marge

--- Ranking/adicionarNomeRanking.js
+++ Ranking/adicionarNomeRanking.js
@@ -2,3 +2,8 @@ var pontos = document.querySelector('#pontos')
 const urlParams = new URLSearchParams(window.location.search);
 const myParam = urlParams.get('pontos');
 pontos.textContent = `${myParam}`;   
+
+var nome = $("#nome");
+nome.focusout( function(){
+    alert(nome.val());
+});
